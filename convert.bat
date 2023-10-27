62fe68152e1459f900ed28ab5f514374d15a2bc3
Georgedf1 <georgedf1@hotmail.co.uk>
2023-10-27 16:31:21 +0100

    Changing to batch file to avoid permissions issues

---
+++ convert.bat
@@ -0,0 +1,2 @@
+SET PATH=%PATH%;C:\Program Files\Blender Foundation\Blender 3.6
+CALL blender.exe empty.blend --background --python convert.py
--- convert.ps1
+++
@@ -1,2 +0,0 @@
-$env:Path += "C:\Program Files\Blender Foundation\Blender 3.6;"
-Start-Process -FilePath "blender.exe" -ArgumentList "empty.blend --background --python convert.py"

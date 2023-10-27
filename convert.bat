fe68f7dc12a2348f81bb0d13738fb8618813d83f
Georgedf1 <georgedf1@hotmail.co.uk>
2023-10-27 18:01:57 +0100

    Implemented rough import-export. But still has UE import issues

--- convert.bat
+++ convert.bat
@@ -1,2 +1,2 @@
 SET PATH=%PATH%;C:\Program Files\Blender Foundation\Blender 3.6
-CALL blender.exe empty.blend --background --python convert.py
+CALL blender.exe --background --python convert.py
--- convert.py
+++ convert.py
@@ -2,7 +2,6 @@ import bpy
 import os
 
 
-BLENDER_DIR = "C:\\Program Files\\Blender Foundation\\Blender 3.6"
 BVH_DIR = "C:\\Users\\gf321\\Research\\lafan1\\lafan1\\lafan1"
 FBX_DIR = ".\\output"
 
@@ -23,6 +22,25 @@ if __name__ == "__main__":
     for filename in filenames:
         bvh_filepath = os.path.join(BVH_DIR, filename + '.bvh')
         fbx_filepath = os.path.join(FBX_DIR, filename + '.fbx')
+
+        bpy.ops.wm.read_factory_settings(use_empty=True)
+
         bpy.ops.import_anim.bvh(filepath=bvh_filepath, update_scene_fps=True, update_scene_duration=True)
-        bpy.ops.export_scene.fbx(filepath=fbx_filepath)
-        break
+
+        # Ensure correct scene frame range
+        assert len(bpy.data.actions) == 1
+        frame_range = bpy.data.actions[0].frame_range
+        bpy.context.scene.frame_start = int(frame_range[0])
+        bpy.context.scene.frame_end = int(frame_range[1])
+
+        # Add some mesh data so UE allows import
+        bpy.data.objects[filename].name = "animation"
+        bpy.ops.mesh.primitive_plane_add()
+        bpy.data.objects["Plane"].name = filename
+        bpy.data.objects["animation"].select_set(True)
+        bpy.ops.object.parent_set()  # Parent the "animation" object to cube
+
+        # Export
+        bpy.ops.export_scene.fbx(filepath=fbx_filepath, check_existing=False)
+
+        raise Exception('Need to figure out how to fix UE incorrect frame rate issue')
